c3f57230ac014f68878786679e7a876db15292d4
MoyDeev <lzsamuel0820@hotmail.com>
2025-01-30 22:08:06 -0600

    Sintaxis, variables, tipos de datos, y hola mundo

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/MoyDeev.py
@@ -0,0 +1,14 @@
+string = "Hola"
+int = 3
+float = 3.5
+bool = True
+boolFalse = False
+
+constante = "Hola Mundo"
+
+print(constante)
+
+print(type(string))
+print(type(int))
+print(type(float))
+print(type(bool))
